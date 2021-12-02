0cb03e585ac0788b92ef42b0c0def7980feec628
Dexter <dextersimch@gmail.com>
2021-12-02 22:17:34 +0800

    v3.1 erase bug fixed

--- etch.js
+++ etch.js
@@ -1,5 +1,6 @@
 let rainbow = false;
-let color = "#000000";
+let erase = false;
+let colorSelect = "#000000";
 let arr = [];
 
 const container = document.getElementById("container");
@@ -7,20 +8,23 @@ const colorbtn = document.getElementById("colormode");
 const rainbowbtn = document.getElementById("rainbowmode");
 const eraser = document.getElementById("eraser");
 const clear = document.getElementById("clear");
+const chooseColor = document.getElementById("chooseColor");
 
-colorbtn.addEventListener("click", () => { rainbow = false; })
-rainbowbtn.addEventListener("click", () => { rainbow = true; });
-eraser.addEventListener("click", () => { rainbow = false; color = "#FFFFFF"; });
+let slider = document.getElementById("myRange");
+let output = document.getElementById("dimension");
+let size = parseInt(slider.value);
+output.textContent = slider.value + "x" + slider.value;
+
+chooseColor.addEventListener("input", () => { colorSelect = chooseColor.value; });
+colorbtn.addEventListener("click", () => { rainbow = false; erase = false; });
+rainbowbtn.addEventListener("click", () => { rainbow = true; erase = false; });
+eraser.addEventListener("click", () => { rainbow = false; erase = true; });
 clear.addEventListener("click", clearAll);
 
 function clearAll(){
     for (let i = 0; i < arr.length; i++){
         arr[i].style.backgroundColor = "#FFFFFF";
     }
-}
-
-function colorSelected(element){
-    color = element.value;
 }
 
 function setup(size){
@@ -37,10 +41,11 @@ function setup(size){
             div.appendChild(pixel);
             pixel.addEventListener("mouseover", hoverfn);
             arr.push(pixel);
-            pixel.style.width = JSON.stringify(600/size) + "px";
-            pixel.style.height = JSON.stringify(600/size) + "px";
+            pixel.style.width = JSON.stringify(620/size) + "px";
+            pixel.style.height = JSON.stringify(620/size) + "px";
 
         }
+
         container.appendChild(div);
     }
 }
@@ -48,15 +53,12 @@ function setup(size){
 function hoverfn(){
     if (rainbow){
         this.style.backgroundColor = "#" + (Math.floor(Math.random() * 16777215)).toString(16);
+    } else if (erase) {
+        this.style.backgroundColor = "#FFFFFF";
     } else {
-        this.style.backgroundColor = color;
+        this.style.backgroundColor = colorSelect;
     }
 }
-
-let slider = document.getElementById("myRange");
-let output = document.getElementById("dimension");
-let size = parseInt(slider.value);
-output.textContent = slider.value + "x" + slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = () => {
